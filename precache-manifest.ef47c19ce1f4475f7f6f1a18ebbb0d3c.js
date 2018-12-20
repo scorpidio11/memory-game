9c9d4842fdb5fb9387a5b1cddfdddfd262c79393
scorpidio11 <scorpido@gmail.com>
2018-12-19 19:28:31 -0700

    Updates

---
+++ precache-manifest.ef47c19ce1f4475f7f6f1a18ebbb0d3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "1a0d587d85d1cbabebcf",
+    "url": "/memory-game/static/js/main.1a0d587d.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/memory-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "1a0d587d85d1cbabebcf",
+    "url": "/memory-game/static/css/main.78f02a5c.chunk.css"
+  },
+  {
+    "revision": "94cd017307677ca3569246505b76341c",
+    "url": "/memory-game/index.html"
+  }
+];
